382edbdf265a34af4d13fd2453b1e0ad643aab4e
DK\rbi <beardedmanul@gmail.com>
2017-05-08 14:06:35 +0300

    Commit patch

---
+++ src/Sitecore.Support.94731/Analytics/Rules/Conditions/LatitudeCondition.cs
@@ -0,0 +1,55 @@
+﻿namespace Sitecore.Support.Analytics.Rules.Conditions
+{
+    using Sitecore.Analytics;
+    using Sitecore.Rules.Conditions;
+    using Sitecore.Rules;
+
+    public class LatitudeCondition<T> : OperatorCondition<T> where T : RuleContext
+    {
+        protected override bool Execute(T ruleContext)
+        {
+            if (Tracker.IsActive == false)
+            {
+                Tracker.StartTracking();
+            }
+
+            double latitude = this.Latitude;
+
+            switch (base.GetOperator())
+            {
+                case ConditionOperator.Equal:
+                    return (latitude == this.Value);
+
+                case ConditionOperator.GreaterThanOrEqual:
+                    return (latitude >= this.Value);
+
+                case ConditionOperator.GreaterThan:
+                    return (latitude > this.Value);
+
+                case ConditionOperator.LessThanOrEqual:
+                    return (latitude <= this.Value);
+
+                case ConditionOperator.LessThan:
+                    return (latitude < this.Value);
+
+                case ConditionOperator.NotEqual:
+                    return !(latitude == this.Value);
+            }
+            return false;
+        }
+
+        public double Latitude
+        {
+            get
+            {
+                if (Tracker.Current.Session.Interaction.HasGeoIpData && Tracker.Current.Session.Interaction.GeoData.Latitude.HasValue)
+                {
+                    return Tracker.Current.Session.Interaction.GeoData.Latitude.GetValueOrDefault();
+                }
+                return double.NaN;
+            }
+        }
+
+        public double Value { get; set; }
+    }
+}
---
+++ src/Sitecore.Support.94731/Analytics/Rules/Conditions/LongitudeCondition.cs
@@ -0,0 +1,55 @@
+﻿namespace Sitecore.Support.Analytics.Rules.Conditions
+{
+    using Sitecore.Analytics;
+    using Sitecore.Rules;
+    using Sitecore.Rules.Conditions;
+
+    public class LongitudeCondition<T> : OperatorCondition<T> where T : RuleContext
+    {
+        protected override bool Execute(T ruleContext)
+        {
+            if (Tracker.IsActive == false)
+            {
+                Tracker.StartTracking();
+            }
+
+            double longitude = this.Longitude;
+
+            switch (base.GetOperator())
+            {
+                case ConditionOperator.Equal:
+                    return (longitude == this.Value);
+
+                case ConditionOperator.GreaterThanOrEqual:
+                    return (longitude >= this.Value);
+
+                case ConditionOperator.GreaterThan:
+                    return (longitude > this.Value);
+
+                case ConditionOperator.LessThanOrEqual:
+                    return (longitude <= this.Value);
+
+                case ConditionOperator.LessThan:
+                    return (longitude < this.Value);
+
+                case ConditionOperator.NotEqual:
+                    return !(longitude == this.Value);
+            }
+            return false;
+        }
+
+        public double Longitude
+        {
+            get
+            {
+                if (Tracker.Current.Session.Interaction.HasGeoIpData && Tracker.Current.Session.Interaction.GeoData.Longitude.HasValue)
+                {
+                    return Tracker.Current.Session.Interaction.GeoData.Longitude.GetValueOrDefault();
+                }
+                return double.NaN;
+            }
+        }
+
+        public double Value { get; set; }
+    }
+}
